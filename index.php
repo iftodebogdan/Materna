b933ef601c39db8320dd321bec125786f06e9a4f
Iftode Bogdan-Marius <iftode.bogdan@gmail.com>
2014-07-13 01:16:21 +0300

    General: 	- modified folder structures and separated engine logic from app logic 	- the URL is now parsed using mod_rewrite so as to make it shorter and more user-friendly 	- support for multiple URL parameters is now added (www.hostname.com/page/param1/param2/etc)

--- index.php
+++ index.php
@@ -10,36 +10,54 @@
 require_once('Utils/Debug/DbgHelper.inc');
 use \Utils\Debug\DbgHelper as DbgHelper;
 
-//Check if there is a PATH_INFO index
-if(!array_key_exists('PATH_INFO', $_SERVER))
-    DbgHelper::printDbgMsgAndDie('Index PATH_INFO does not exist in $_SERVER!', DbgHelper::ERROR);
+//Include Cfg class to access configuration data
+require_once('Resources/Config/cfg.inc');
+use \Resources\Config\Cfg as Cfg;
 
-//Parse the PATH_INFO
-$userArgs = explode('/', $_SERVER['PATH_INFO']);
+//Check if there is a QUERY_STRING index
+if(!array_key_exists('QUERY_STRING', $_SERVER) || !strlen($_SERVER['QUERY_STRING']))
+    DbgHelper::printDbgMsgAndDie('Index QUERY_STRING does not exist in $_SERVER!', DbgHelper::ERROR);
+
+//Parse the QUERY_STRING
+$params = explode('/', $_SERVER['QUERY_STRING']);
 
 //Sanitize input
-$userArgs = array_filter($userArgs, 'strlen');
+$params = array_filter($params, 'strlen');
 
 //Check if the user arguments are valid
-if( count($userArgs) < 1 )
-    DbgHelper::printDbgMsgAndDie('$userArgs is empty!', DbgHelper::ERROR);
+if( count($params) )
+{
+    //Build class names
+    $modelClass = $params[0] . 'Model';
+    $viewClass = $params[0] . 'View';
+    $controllerClass = $params[0] . 'Controller';
 
-//Build class names
-$modelClass = $userArgs[1] . 'Model';
-$viewClass = $userArgs[1] . 'View';
-$controllerClass = $userArgs[1] . 'Controller';
+    //Check if the files exist
+    if( file_exists('Apps/' . Cfg::$appTitle . '/Models/' . $modelClass . '.inc') &&
+        file_exists('Apps/' . Cfg::$appTitle . '/Views/' . $viewClass . '.inc') &&
+        file_exists('Apps/' . Cfg::$appTitle . '/Controllers/' . $controllerClass . '.inc') )
+    {
+        //Include class definitions
+        require_once('Apps/' . Cfg::$appTitle . '/Models/' . $modelClass . '.inc');
+        require_once('Apps/' . Cfg::$appTitle . '/Views/' . $viewClass . '.inc');
+        require_once('Apps/' . Cfg::$appTitle . '/Controllers/' . $controllerClass . '.inc');
 
-//Include class definitions
-require_once("Models/$modelClass.inc");
-require_once("Views/$viewClass.inc");
-require_once("Controllers/$controllerClass.inc");
 
-//Add namespaces to class names
-$modelClass = '\Models\\' . $modelClass;
-$viewClass = '\Views\\' . $viewClass;
-$controllerClass = '\Controllers\\' . $controllerClass;
+        //Add namespaces to class names
+        $modelClass = '\Models\\' . $modelClass;
+        $viewClass = '\Views\\' . $viewClass;
+        $controllerClass = '\Controllers\\' . $controllerClass;
 
-//Instantiate modules
-$model = new $modelClass();
-$controller = new $controllerClass($model);
-$view = new $viewClass($controller, $model);
+        //Instantiate modules
+        $model = new $modelClass() or DbgHelper::printDbgMsgAndDie("Could not instantiate $modelClass!", DbgHelper::SYSTEM);
+        $controller = new $controllerClass($model) or DbgHelper::printDbgMsgAndDie("Could not instantiate $controllerClass!", DbgHelper::SYSTEM);
+        $view = new $viewClass($controller, $model) or DbgHelper::printDbgMsgAndDie("Could not instantiate $viewClass!", DbgHelper::SYSTEM);
+
+    }
+    else
+        //At least one file does not exist
+        DbgHelper::printDbgMsgAndDie('Could not find the \'' . $params[0] . '\' MVC model!', DbgHelper::APP);
+}
+else
+    //There are no user arguments
+    DbgHelper::printDbgMsgAndDie('There are no app parameters!', DbgHelper::ERROR);
